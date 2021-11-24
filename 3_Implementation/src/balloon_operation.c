becb138a1500d31bfd5b2f76839f32ad04884716
Tumaskar <94179036+Tumaskar@users.noreply.github.com>
2021-11-24 12:18:08 +0530

    Update balloon_operation.c

--- 3_Implementation/src/balloon_operation.c
+++ 3_Implementation/src/balloon_operation.c
@@ -9,5 +9,14 @@
  * 
  */
 
+void draw_balloon(int ,int );
+void draw_burst ( int x, int y );
+void draw_bow(int x,int y);
+void draw_arrow(int x, int y);
+void shoot(int *x, int *y);
+int testkeys();
+void fly(int *x, int *y);
+void start();
+
 
 
